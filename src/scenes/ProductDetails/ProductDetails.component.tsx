0be627b3ae6327ef454acac682eba552c1820275
cengiz.ilkerli <cengizilkerli@gmail.com>
2025-02-04 17:03:47 +0300

    kullanılmayan import düzenlemeleri yapıldı

--- src/scenes/ProductDetails/ProductDetails.component.tsx
+++ src/scenes/ProductDetails/ProductDetails.component.tsx
@@ -12,19 +12,34 @@ const ProductDetails = () => {
   const { title, description, image, price, rating } = productDataFromRoute.product
 
   const { } = useProductDetails();
-  const { productContainer, productImage, productTitle, productDescription, productBottomInfoContainer,productPrice,productRating } = styles;
+  
+  const { 
+    productContainer, 
+    productImage, 
+    productTitle, 
+    productDescription, 
+    productBottomInfoContainer,
+    productPrice,
+    productRating 
+  } = styles;
 
   return (
+
     <View style={productContainer}>
+    
       <Image style={productImage} source={{uri : image}} />
       <Text style={productTitle}>{ title }</Text>
       <Text style={productDescription}>{ description }</Text>
       
       <View style={productBottomInfoContainer}>
+
         <Text style={productPrice}>{price}</Text>
         <Text style={productRating}>{rating}</Text>
+
       </View>
+    
     </View>
+
   );
 };
 
--- src/scenes/Products/Products.component.tsx
+++ src/scenes/Products/Products.component.tsx
@@ -1,5 +1,4 @@
 import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
 import styles from './Products.style';
 import useProducts from './hooks/useProducts.hook';
@@ -7,8 +6,7 @@ import useProducts from './hooks/useProducts.hook';
 const Products = () => {
   
   const { products, handleOnProductPress } = useProducts();
-  const { container, productContainer, productImage, productTitle, productDescription, productPrice, productRating, productBottomInfoContainer } = styles;
-  const nagivation = useNavigation();
+  const { productContainer, productImage, productTitle, productDescription, productPrice, productRating, productBottomInfoContainer } = styles;
 
   return (
 
@@ -17,14 +15,15 @@ const Products = () => {
       keyExtractor={item => item.id.toString()}
       contentContainerStyle={{ rowGap: 20, padding: 10 }}
       renderItem={({ item }) => {
+        
         return (
 
           <View key={item.id} style={productContainer}>
+
             <TouchableOpacity 
               key={item.id}
               onPress={() => handleOnProductPress(item)}
               >
-
                 <Image
                   source={{ uri: item.image }}
                   style={productImage}
@@ -33,15 +32,20 @@ const Products = () => {
                 <Text style={productDescription}>{item.description}</Text>
                   
                 <View style={productBottomInfoContainer}>
+
                   <Text style={productPrice}>{item.price}</Text>
                   <Text style={productRating}>{item.rating}</Text>
+
                 </View>
 
               </TouchableOpacity>
+
           </View>
         );
+
       }}
     />
+
   );
 
 };
